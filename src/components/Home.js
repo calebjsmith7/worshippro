92c5e0eeb5197848893b8b209d0dd17e7a67eb2a
Caleb Smith <calebsmith@Calebs-MacBook-Pro-2.local>
2021-05-26 13:41:35 -0600

    email subscription add on

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,18 +27,35 @@ class Home extends Component {
         
 
             <div className="c">
-                <a id="restorations">
                     <h3 className="mods">PATCHES</h3>
                     <HashLink to="/patches"><button className="designyours btn btn-light">EXPLORE</button></HashLink>
-                </a>
             </div>
             <div className="d">
-                
-                <a id="restorations">
                     <h3 className="mods">TUTORIALS</h3>
                     <HashLink to="/tutorials"><button className="designyours btn btn-light">EXPLORE</button></HashLink>
-                </a>
             </div>
+
+            <div className="e">
+                <div className="subscribeleft">
+                    <h3 className="subscribetitle">Stay up to speed on the latest at Worship Pro</h3>
+                </div>
+                <div className="subscriberight">
+
+                    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
+                <div id="mc_embed_signup">
+                 <form action="https://jfc.us6.list-manage.com/subscribe/post?u=3fa7a9607b9b800108da06b8e&amp;id=cae083dab8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+	
+	                    <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="Email Address" required ></input>
+                                <div style={{position: 'absolute', left: -5000 + 'px'}} aria-hidden="true"><input type="text" name="b_3fa7a9607b9b800108da06b8e_cae083dab8" tabindex="-1" value="" ></input></div>
+                                    <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" /></div>
+                    </div>
+                </form>
+                </div>
+
+                </div>
+            </div>
+
         </div>
     );
   }
